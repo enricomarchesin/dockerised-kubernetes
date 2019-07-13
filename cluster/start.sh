62819dfdc6ca1003e64a7d0c53596f70e99df165
Enrico Marchesin <enrico@cainthus.com>
2019-07-13 21:30:18 +0100

    Docker-in-docker

--- cluster/start.sh
+++ cluster/start.sh
@@ -1,15 +1,11 @@
 #!/bins/bash
-# /etc/init.d/docker start
-
-curl -s https://raw.githubusercontent.com/rancher/k3d/master/install.sh | bash
-k3d create --auto-restart --publish 80 --name k3d
-# -v /var/lib/docker:/var/lib/docker -v /var/lib/rancher:/var/lib/rancher
-# --workers 2
+k3d create --api-port docker:6443 --publish 80 --name k3d --workers 1
+sleep 5
 export KUBECONFIG="$(k3d get-kubeconfig --name='k3d')"
 source <(kubectl completion bash)
 
-# kubectl create deployment nginx --image=nginx
-# kubectl create service clusterip nginx --tcp=80:80
-# kubectl apply -f ingress.yml
+kubectl create deployment nginx --image=nginx
+kubectl create service clusterip nginx --tcp=80:80
+kubectl apply -f ingress.yml
 
 /setup/ttyd -p 8022 bash
